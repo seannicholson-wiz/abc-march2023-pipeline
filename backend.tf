cbbf9b8c7fc318e33b962c9ac88efebb44577d21
sean nicholson <seannicholson@seans-MacBook-Pro.local>
2023-03-22 16:00:01 -0500

    updates to backend.tf

--- backend.tf
+++ backend.tf
@@ -4,7 +4,7 @@ terraform {
   backend "azurerm" {
     subscription_id      = "243320b7-89a5-4585-ab9c-d3a99ba5a179"
     resource_group_name  = "wizDemoResourceGroup"
-    storage_account_name = "wizpipelinestorage"
+    storage_account_name = "pipelinesean21468"
     container_name       = "terraformstate"
     key                  = "root.terraform.tfstate"
   }
